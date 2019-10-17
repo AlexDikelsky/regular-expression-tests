9c50c37730434de48f4598ff00e1c507ed91d5dc
Alex Dikelsky <alexander.dikelsky@gmail.com>
2019-10-17 16:21:39 -0500

    Deleted failed attempts

--- main.rs
+++ main.rs
@@ -1,5 +1,3 @@
-use combinations::Combinations;
-use regex::Regex;
 use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
 
@@ -9,22 +7,6 @@ fn main() {
     try_regex();
 
 }  
-//{{{
-//fn perm(length: usize, characters: Vec<char>) -> Vec<char> { //Permutes all possible 2, 3, and 4 character combos of "10*."  //{{{
-//    match length == 1 {
-//        true => vec![characters[0]],
-//        false => {
-//            for i in characters.iter() {
-//                println!("{}", i);
-//                perm(length-1, 
-//            }
-//            characters
-//            //perm(length-1, vec![characters[2]])
-//        }
-//    }
-//} //}}}
-//
-
 
 fn to_octal() -> io::Result<()> { // {{{
     let file = File::open("octal_nums.txt")?;
@@ -50,38 +32,6 @@ fn to_octal() -> io::Result<()> { // {{{
     Ok(())
 } //}}}
 
-fn try_regex() -> io::Result<()> {
-    let file = File::open("eight_regex.txt")?;
-    let reader = BufReader::new(file);
-
-    for line in reader.lines() {
-        test_regex(&("^".to_owned() + &line.unwrap() + "$"));
-    }
-    Ok(())
-}
-    
-
-
-fn test_regex(reg_str: &str) -> io::Result<()> {
-    let file = File::open("nums.txt")?;
-    let reader = BufReader::new(file);
-
-    let re = Regex::new(reg_str).unwrap();
-
-    for line in reader.lines() {
-        print!("{:?}", line);
-        println!("{}", re.is_match(&line.unwrap()));
-        //
-        //let a = match &line.Ok() {
-        //    true => {
-        //        println!("{}", re.is_match(&line.unwrap()));
-        //    },
-        //    false => {println!("{}", "Error");},
-        //};
-    }
-
-    Ok(())
-}
 
 fn print_nums(power: u32) -> () {
     for i in 0..(2_usize.pow(power)) {
